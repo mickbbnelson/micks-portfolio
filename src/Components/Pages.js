90af1f56ca63b58c161f4d853af28bb5e02bcd94
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-02-07 20:44:41 -0600

    Add pages component

---
+++ src/Components/Pages.js
@@ -0,0 +1,13 @@
+import { Switch } from 'react-router-dom'
+
+const Pages = () => {
+    return (
+        <div>
+        <Switch>
+
+        </Switch>
+        </div>
+    )
+}
+
+export default Pages
