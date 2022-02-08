b92649d6f3532d891061533bb3cd057341404332
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-02-07 21:27:28 -0600

    Add links to navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom'
+
 const NavBar = () => {
   
     return (
         <div id="navbar">
-            
+            <Link to='/about' id="nav-link-about">About</Link>
+            <Link to='/contact' id="nav-link-contact">Contact</Link>
+            <Link to='/projects' id="nav-link-projects">Projects</Link>
         </div>
     )
 
 }
+
+export default NavBar
--- src/Components/Pages.js
+++ src/Components/Pages.js
@@ -1,10 +1,15 @@
 import { Switch } from 'react-router-dom'
+import About from './About'
+import Contact from './Contact'
+import Projects from './Projects'
 
 const Pages = () => {
     return (
         <div>
         <Switch>
-
+            <Route exact path="/about"><About/> </Route>
+            <Route exact path="/contact"><Contact/> </Route>
+            <Route exact path="/projects"><Projects/> </Route>
         </Switch>
         </div>
     )
