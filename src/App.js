5548dd76659669fbda7a9c0efed40d27353a100e
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-02-07 21:43:24 -0600

    Add links to NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <div>
-      
+      <NavBar />
     </div>
   );
 }
