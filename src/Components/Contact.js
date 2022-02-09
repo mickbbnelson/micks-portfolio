045c764e77f99103e9804197c36fa0620256d6b4
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2022-02-09 17:52:39 -0600

    Add links to Contact Page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,6 +3,9 @@ const Contact = () => {
     return (
         <div>
             <h1>Contact</h1>
+            <a href="https://github.com/mickbbnelson">GitHub</a><br></br>
+            <a href="https://www.linkedin.com/in/mick-nelson-denver/">LinkedIn</a><br></br>
+            <a href="mailto: mickbbnelson@gmail.com">Email</a>
         </div>
     )
 }
